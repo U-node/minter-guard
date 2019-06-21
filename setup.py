6c506b51d4f39bc754af939b706e65c6d8d8a77d
Roman <rymka1989@gmail.com>
2019-06-21 04:13:01 +0300

    Setup created

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import codecs
+
+with codecs.open("README.md", "r", 'utf_8_sig') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="minterguard",
+    version="1.0.0",
+    author="U-node Team",
+    author_email="rymka1989@gmail.com",
+    description=u"Python Guard for Minter Nodes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/U-node/minter-guard",
+    packages=setuptools.find_packages(include=['minterguard']),
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
